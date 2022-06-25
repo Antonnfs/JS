ff69fd8ed46faad22c234de56ccaa439a132197e
Anton Pylypenko <90262463+Antonnfs@users.noreply.github.com>
2022-06-25 10:12:32 +0300

    Update script.js
    Signed-off-by: Anton Pylypenko <90262463+Antonnfs@users.noreply.github.com>

--- JS-3/script.js
+++ JS-3/script.js
@@ -1,4 +1,20 @@
 // Task #1
+/**
+  * Описание задачи: Напишите функцию, которая делает поверхностную проверку объекта на пустоту.
+  * Ожидаемый результат: ({}) => true,
+      ({ a: undefined }) => true,
+      ({ a: 1 }) => false
+  * Пустые значения: '', null, NaN, undefined
+  * Сложность задачи: 2 of 5
+  * @param {Object} object - объект с примитивами
+  * @returns {boolean}
+
+const data = { a: 1, b: undefined };
+const data2 = { a: undefined };
+console.log(isEmpty(data)); // false
+console.log(isEmpty(data2)); // true
+*/
+
 const isEmpty = function(object) {
 	let result;
 	const trueValues = [];
@@ -21,6 +37,20 @@ console.log(isEmpty(data1)); // false
 console.log(isEmpty(data2)); // true
 
 // Task #2
+/**
+  * Описание задачи: Напишите функцию, которая делает глубокое сравнение объектов.
+  * Ожидаемый результат: True если объекты идентичны ({ a: 1, b: { c: 1 } }, { a: 1, b: { c: 1 } }) => true
+  * @param {Object} firstObj - Объект с любыми значениями
+  * @param {Object} secondObj - Объект с любыми значениями
+  * @returns {boolean}
+  
+const data = { a: 1, b: { c: 1 } };
+const data2 = { a: 1, b: { c: 1 } };
+const data3 = { a: 1, b: { c: 2 } };
+console.log(isEqualDeep(data, data2)); // true
+console.log(isEqualDeep(data, data3)); // false
+*/
+
 function isEqualDeep(object1, object2) {
 	return JSON.stringify(object1) === JSON.stringify(object2);
 }
@@ -32,6 +62,19 @@ console.log(isEqualDeep(data3, data5)); // false
 
 
 // Task #3
+/**
+  * Описание задачи: Напишите функцию, которая поверхностно находит пересечения объектов и возвращает объект пересечений.
+  * Ожидаемый результат: ({ a: 1, b: 2 }, { c: 1, b: 2 }) => { b: 2 }
+  * @param {Object<string | number>} firstObj - объект с примитивными значениями
+  * @param {Object<string | number>} secondObj - объект с примитивными значениями
+  * @returns {Object}
+
+const data = { a: 1, b: 2 };
+const data2 = { c: 1, b: 2 };
+console.log(intersection(data, data2)); // { b: 2 }
+*/
+
+
 const intersection = function(firstObject, secondObject) {
 	const intersectionObject = {};
 	for(let prop in firstObject) {
